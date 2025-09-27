f7ccc51b5db7ebea30389b2122cc64f968f1b9ae
PengiunCho <ms0223900@gmail.com>
2025-09-27 11:19:05 +0800

    更新 Jest 配置，新增測試超時設定以提升測試穩定性。

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
+  testTimeout: 2000,
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
